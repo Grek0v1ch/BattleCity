a063d7725eaa8cad9dbd5ac40aedcd80d1501e32
Ivan Voronov <ivvoronov2308@gmail.com>
2023-01-03 00:15:33 +0300

    Рефакторинг

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 int main(void)
 {
     GLFWwindow* window;
@@ -8,6 +10,13 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -21,14 +30,18 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     if (! gladLoadGL()) {
+        std::cout << "Can't load GLAD" << std::endl;
         return -1;
     }
+
+    std::cout << "OpenGL " << GLVersion.major << ' ' << GLVersion.minor << '\n';
+    glClearColor(1, 1, 0 , 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
