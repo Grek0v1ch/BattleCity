d05dcbc9d85a62922b827e6db69da9f88d938db9
Ivan Voronov <ivvoronov2308@gmail.com>
2023-01-03 11:38:03 +0300

    Создание окна OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 
 #include <iostream>
 
-int main(void)
-{
-    GLFWwindow* window;
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
 
+int main() {
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cout << "GLFW not initialized" << std::endl;
         return -1;
+    }
 
+    // Установка версии OpenGL и Core-профиля
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -19,32 +22,39 @@ int main(void)
 #endif
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY,
+                                           "Battle City", nullptr, nullptr);
+    if (! pWindow) {
+        std::cout << "Window not initialized" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow);
 
     if (! gladLoadGL()) {
         std::cout << "Can't load GLAD" << std::endl;
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << ' ' << GLVersion.minor << '\n';
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+#ifndef __APPLE__
+    std::cout << "Renderer: " << glGetString(GL_RENDER) << std::endl;
+#else
+    std::cout << "Renderer: Apple M1" << std::endl;
+#endif
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
     glClearColor(1, 1, 0 , 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (! glfwWindowShouldClose(pWindow)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
