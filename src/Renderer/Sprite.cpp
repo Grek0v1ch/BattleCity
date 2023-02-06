131912f7363212ba7b387a64efb5c86f3f6dad1d
Ivan Voronov <ivvoronov2308@gmail.com>
2023-02-06 12:09:14 +0300

    Теперь отрисовка происходит через индексный буфер.

--- src/Renderer/Sprite.cpp
+++ src/Renderer/Sprite.cpp
@@ -19,18 +19,15 @@ namespace Renderer {
                    m_size(size),
                    m_rotation(rotation) {
         const GLfloat vertexCoords[] {
-            // 2--3  4
-            // | / / |
-            // 1  6--5
+            // 1--2
+            // |  |
+            // 0--3
 
             // x    y
-            0.0f, 0.0f, // 1
-            0.0f, 1.0f, // 2
-            1.0f, 1.0f, // 3
-
-            1.0f, 1.0f, // 4
-            1.0f, 0.0f, // 5
-            0.0f, 0.0f  // 6
+            0.0f, 0.0f, // 0
+            0.0f, 1.0f, // 1
+            1.0f, 1.0f, // 2
+            1.0f, 0.0f, // 3
         };
 
         auto subTexture = m_pTexture->getSubTexture(initialSubTexture);
@@ -40,10 +37,12 @@ namespace Renderer {
             subTexture.leftBottomUV.x, subTexture.leftBottomUV.y,
             subTexture.leftBottomUV.x, subTexture.rightTopUV.y,
             subTexture.rightTopUV.x, subTexture.rightTopUV.y,
-
-            subTexture.rightTopUV.x, subTexture.rightTopUV.y,
             subTexture.rightTopUV.x, subTexture.leftBottomUV.y,
-            subTexture.leftBottomUV.x, subTexture.leftBottomUV.y,
+        };
+
+        const GLint indices[] {
+            0, 1, 2,
+            2, 3, 0
         };
 
         glGenVertexArrays(1, &m_VAO);
@@ -61,13 +60,19 @@ namespace Renderer {
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
+        glGenBuffers(1, &m_EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 
     Sprite::~Sprite() {
         glDeleteBuffers(1, &m_vertexCoordsVBO);
         glDeleteBuffers(1, &m_textureCoordsVBO);
+        glDeleteBuffers(1, &m_EBO);
         glDeleteVertexArrays(1, &m_VAO);
     }
 
@@ -87,7 +92,7 @@ namespace Renderer {
 
         glActiveTexture(GL_TEXTURE0);
         m_pTexture->bind();
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         glBindVertexArray(0);
     }
 
